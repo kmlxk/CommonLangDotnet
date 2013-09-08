32e2c07191bd9e3f9383bfbb37d75b4c3d747cd6
kmlxk0 <kmlxk0@gmail.com>
2013-09-08 12:16:59 +0800

    为DictionaryHelper增加了一些类型转换的方法

--- CommonLang/DictionaryHelper.cs
+++ CommonLang/DictionaryHelper.cs
@@ -20,7 +20,43 @@ namespace CommonLang
         public DictionaryHelper()
 		{
 		}
-		
+
+        public static TValue[][] toArray2d(List<Dictionary<TKey, TValue>> list)
+        {
+            List<TValue[]> rows = new List<TValue[]>();
+            List<TValue> listHeaders = new List<TValue>();
+            foreach (Dictionary<TKey, TValue> obj in list)
+            {
+                if (listHeaders.Count == 0)
+                {
+                    foreach (TKey key in obj.Keys)
+                    {
+                        listHeaders.Add((TValue)Convert.ChangeType(key, typeof(TValue)));
+                    }
+                    rows.Add(listHeaders.ToArray());
+                }
+                TValue[] row = new TValue[listHeaders.Count];
+                int col = 0;
+                foreach (TValue val in obj.Values)
+                {
+                    row[col] = val;
+                }
+                rows.Add(row);
+            }
+            return rows.ToArray();
+        }
+
+        // 字符串化
+        public static Dictionary<string, string> toString(Dictionary<TKey, TValue> dict)
+        {
+            Dictionary<string, string> ret = new Dictionary<string, string>();
+            foreach (KeyValuePair<TKey, TValue> kv in dict)
+            {
+                ret.Add(kv.Key.ToString(), kv.Value.ToString());
+            }
+            return ret;
+        }
+
         // 差集
 		public static Dictionary<TKey, TValue> except(Dictionary<TKey, TValue> dict, List<TKey> list)
 		{
@@ -44,6 +80,18 @@ namespace CommonLang
 			}
 			return ret;
 		}
-	
-	}
+
+
+        public static Dictionary<T1, T2> changeType<T1, T2>(Dictionary<TKey, TValue> dict)
+        {
+            Dictionary<T1, T2> ret = new Dictionary<T1, T2>();
+            foreach (KeyValuePair<TKey, TValue> kv in dict)
+            {
+                ret.Add((T1)Convert.ChangeType(kv.Key, typeof(T1)), 
+                    (T2)Convert.ChangeType(kv.Value, typeof(T2))
+                    );
+            }
+            return ret;
+        }
+    }
 }
